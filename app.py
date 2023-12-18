d741b6ab0d922d912bc2e7113e4f03622b2a5789
the only one <106877000+yousefessam44034@users.noreply.github.com>
2023-12-18 13:27:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,73 @@ def connect_to_mysql():
         password=db_password,
         database=db_name
     )
+    @app.route('/check_username', methods=['POST'])
+def check_username():
+    data = request.get_json()
+    username = data.get('username')
+
+    connection = None
+
+    try:
+        connection = connect_to_mysql()
+        cursor = connection.cursor()
+
+        # Check if the username already exists in the database
+        query = "SELECT * FROM users WHERE username = %s"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+
+        if result:
+            return jsonify({"message": "Username already exists"})
+
+        return jsonify({"message": "Username available"})
+
+    except mysql.connector.Error as error:
+        print("Error: ", error)
+        return jsonify({"message": f"Error checking username. Error: {error}"})
+
+    finally:
+        if connection:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    user_type = data.get('user_type')  # Add user_type field (doctor or patient)
+
+    connection = None
+
+    try:
+        connection = connect_to_mysql()
+        cursor = connection.cursor()
+
+        # Check if the username already exists in the database
+        query = "SELECT * FROM users WHERE username = %s"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+
+        if result:
+            return jsonify({"message": "Username already exists"})
+
+        query = "INSERT INTO users (username, password, user_type) VALUES (%s, %s, %s)"
+        cursor.execute(query, (username, password, user_type))
+        connection.commit()
+
+        return jsonify({"message": "Registration successful"})
+
+    except mysql.connector.Error as error:
+        print("Error: ", error)
+        return jsonify({"message": f"Registration failed. Error: {error}"})
+
+    finally:
+        if connection:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
 
 if __name__ == '__main__':
     port = int(os.environ.get('FLASK_PORT') or 8080)
