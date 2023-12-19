481aa2115729dc4d0587ede058195dccb7ee75af
the only one <106877000+yousefessam44034@users.noreply.github.com>
2023-12-19 18:34:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,134 @@ def register():
                 cursor.close()
                 connection.close()
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    connection = None
+
+    try:
+        connection = connect_to_mysql()
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username = %s"
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+
+        if not result:
+            return jsonify({"message": "Login failed"})
+
+        stored_password = result[2]  # Assuming the password field is at index 2
+        if password == stored_password:
+            user_type = result[3]  # Sending user_type
+            return jsonify({"message": "Login successful", "user_type": user_type, "username": username})
+
+        return jsonify({"message": "Login failed"})
+
+    except mysql.connector.Error as error:
+        print("Error: ", error)
+        return jsonify({"message": "Login failed"})
+
+    finally:
+        if connection:
+            if connection is not None:
+                cursor.close()
+                connection.close()
+
+                
+@app.route('/insert_doctor_slots', methods=['POST'])
+def insert_doctor_slots():
+    data = request.get_json()
+    doctor_username = data.get('doctor_username')
+    day_of_week = data.get('day_of_week')
+    time_slot = data.get('time_slot')
+    status = data.get('status')  # You can specify 'booked' or 'not_booked' for status
+
+    # Perform an authorization check here
+    user_type = get_user_type(doctor_username)  # Replace with your function to retrieve user type
+
+    if user_type != 'doctor':
+        return jsonify({"message": "Unauthorized access"})
+
+    connection = None
+
+    try:
+        connection = connect_to_mysql()
+        cursor = connection.cursor()
+
+        # Insert a new slot into the doctor_slots table
+        query = "INSERT INTO doctor_slots (doctor_username, day_of_week, time_slot, status) VALUES (%s, %s, %s, %s) "
+        cursor.execute(query, (doctor_username, day_of_week, time_slot, status))
+        connection.commit()
+
+        return jsonify({"message": "Doctor's slot added successfully"})
+
+    except mysql.connector.Error as error:
+        print("Error: ", error)
+        return jsonify({"message": "Failed to add doctor's slot"})
+
+    finally:
+        if connection:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+                
+
+def get_user_type(username):
+    # Function to retrieve the user type based on the username
+    connection = connect_to_mysql()
+    cursor = connection.cursor()
+    query = "SELECT user_type FROM users WHERE username = %s"
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
+    connection.close()
+
+    if result:
+        return result[0]
+    else:
+        return None
+    
+
+@app.route('/patient_appointment', methods=['POST'])
+def patient_appointment():
+    data = request.get_json()
+    patient_username = data.get('patient_username')
+    doctor_username = data.get('doctor_username')
+
+    connection = connect_to_mysql()
+    cursor = connection.cursor()
+
+    # Step 1: View available doctors
+    query = "SELECT username FROM users WHERE user_type = 'doctor'"
+    cursor.execute(query)
+    doctors = [doctor[0] for doctor in cursor.fetchall()]
+
+    # Step 2: View available slots of the selected doctor
+    query = "SELECT day_of_week, time_slot, status FROM doctor_slots WHERE doctor_username = %s"
+    cursor.execute(query, (doctor_username,))
+    slots = cursor.fetchall()
+    slot_info = [{"day_of_week": slot[0], "time_slot": slot[1], "status": slot[2]} for slot in slots]
+
+    # Step 3: Choose a slot
+    day_of_week = data.get('day_of_week')
+    time_slot = data.get('time_slot')
+
+    # Insert a new appointment record
+    query = "INSERT INTO appointments (doctor_username, patient_username, day_of_week, time_slot) VALUES (%s, %s, %s, %s)"
+    cursor.execute(query, (doctor_username, patient_username, day_of_week, time_slot))
+
+    # Update the status of the slot to 'booked'
+    update_query = "UPDATE doctor_slots SET status = 'booked' WHERE doctor_username = %s AND day_of_week = %s AND time_slot = %s"
+    cursor.execute(update_query, (doctor_username, day_of_week, time_slot))
+
+    connection.commit()
+    connection.close()
+
+    return jsonify({"message": "Appointment booked successfully", "doctors": doctors, "slots": slot_info})
+
 # ... (your other routes)
 
 if __name__ == '__main__':
